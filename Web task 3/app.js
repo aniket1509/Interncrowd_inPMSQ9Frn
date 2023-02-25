3341b6165a2f0fe5a290856822622efbc6ea8f8b
Aniket <aniket.n.rawat@gmail.com>
2023-02-26 00:16:50 +0530

    third task

---
+++ Web task 3/app.js
@@ -0,0 +1,96 @@
+const quizData = [{
+    question: "JavaScript is an ______ language",
+    a: "Object Oriented",
+    b: "Object-based",
+    c: "Procedural",
+    d: "None pf the above",
+    correct: "b",
+},
+{
+    question: "Which one of the following also known as Conditional Expression:",
+    a: "Alternative to if-else",
+    b: "Switch statement",
+    c: "if-then-else statement",
+    d: "immediate if",
+    correct: "d",
+},
+{
+    question: "What year was JavaScript launched?",
+    a: "1996",
+    b: "1995",
+    c: "1994",
+    d: "none of the above",
+    correct: "b",
+},
+{
+    question: "In JavaScript, what is a block of statement?",
+    a: "Conditional block",
+    b: "block that combines a number of statements into a single compound statement",
+    c: "both conditional block and a single statement",
+    d: "block that contains a single statement",
+    correct: "b",
+}
+];
+let index = 0;
+let correct = 0,
+incorrect = 0,
+total = quizData.length;
+let questionBox = document.getElementById("questionBox");
+let allInputs = document.querySelectorAll("input[type='radio']")
+const loadQuestion = () => {
+if (total === index) {
+    return quizEnd()
+}
+reset()
+const data = quizData[index]
+questionBox.innerHTML = `${index + 1}) ${data.question}`
+allInputs[0].nextElementSibling.innerText = data.a
+allInputs[1].nextElementSibling.innerText = data.b
+allInputs[2].nextElementSibling.innerText = data.c
+allInputs[3].nextElementSibling.innerText = data.d
+}
+
+document.querySelector("#submit").addEventListener(
+"click",
+function() {
+    const data = quizData[index]
+    const ans = getAnswer()
+    if (ans === data.correct) {
+        correct++;
+    } else {
+        incorrect++;
+    }
+    index++;
+    loadQuestion()
+}
+)
+
+const getAnswer = () => {
+let ans;
+allInputs.forEach(
+    (inputEl) => {
+        if (inputEl.checked) {
+            ans = inputEl.value;
+        }
+    }
+)
+return ans;
+}
+
+const reset = () => {
+allInputs.forEach(
+    (inputEl) => {
+        inputEl.checked = false;
+    }
+)
+}
+
+const quizEnd = () => {
+// console.log(document.getElementsByClassName("container"));
+document.getElementsByClassName("container")[0].innerHTML = `
+    <div class="col">
+        <h3 class="w-100"> Your score: ${correct} / ${total} </h3>
+    </div>
+`
+}
+loadQuestion(index);
